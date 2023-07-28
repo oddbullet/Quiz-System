fa81a4e8e2fb15531460dddbba4ea1cd290cf54b
Bill Yang <joyang12345@gmail.com>
2023-07-28 14:39:25 -0400

    created new grid and toolbar

--- src/main/java/com/example/application/views/helloworld/HomeView.java
+++ src/main/java/com/example/application/views/helloworld/HomeView.java
@@ -1,35 +1,48 @@
 package com.example.application.views.helloworld;
 
+import com.example.application.data.QuestionSet;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 
 @PageTitle("Home")
-@Route(value = "hello", layout = MainLayout.class)
+@Route(value = "home", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
-public class HelloWorldView extends HorizontalLayout {
+public class HomeView extends VerticalLayout {
+    Grid<QuestionSet> grid = new Grid<>(QuestionSet.class);
+    TextField filterText = new TextField();
 
-    private TextField name;
-    private Button sayHello;
+    public HomeView() {
+        addClassName("list-view");
+        setSizeFull();
 
-    public HelloWorldView() {
-        name = new TextField("Filter");
-        sayHello = new Button("New Set");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        configureGrid();
+        add(getToolbar(), grid);
     }
 
+    private void configureGrid() {
+        grid.addClassName("questionSet-grid");
+        grid.setSizeFull();
+        grid.setColumns("name", "numberOfQuestion");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private HorizontalLayout getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button newSetButton = new Button("New Set");
+        var toolbar = new HorizontalLayout(filterText, newSetButton);
+        return toolbar;
+    }
 }

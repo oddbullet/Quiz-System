6b35704fabeda74c42a751f9e62802a28cb88ea8
Bill Yang <joyang12345@gmail.com>
2023-07-28 14:38:36 -0400

    new entity question set

--- src/main/java/com/example/application/data/QuestionSet.java
+++ src/main/java/com/example/application/data/QuestionSet.java
@@ -1,4 +1,65 @@
 package com.example.application.data;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class QuestionSet {
+
+    @Id
+    @SequenceGenerator(name = "question_set_seq", sequenceName = "QUESTION_SET_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "question_seq")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "question_set_id")
+    private Set<Question> questions = new HashSet<>();
+
+    private int numberOfQuestion;
+
+    public QuestionSet(String name) {
+        this.name = name;
+    }
+
+    public QuestionSet(String name, Set<Question> questions) {
+        this.name = name;
+        this.questions = questions;
+    }
+
+    public QuestionSet(String name, Set<Question> questions, int numberOfQuestion) {
+        this.name = name;
+        this.questions = questions;
+        this.numberOfQuestion = numberOfQuestion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        this.questions = questions;
+    }
+
+    public int getNumberOfQuestion() {
+        return numberOfQuestion;
+    }
+
+    public void setNumberOfQuestion(int numberOfQuestion) {
+        this.numberOfQuestion = numberOfQuestion;
+    }
 }
+
+

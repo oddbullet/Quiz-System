e5335abf7abda33eee1e9bd4261730c6491991f0
Bill Yang <joyang12345@gmail.com>
2023-08-01 15:43:10 -0400

    added repository annotation

--- src/main/java/com/example/application/data/repository/QuestionRepository.java
+++ src/main/java/com/example/application/data/repository/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.example.application.data.repository;
 
 import com.example.application.data.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }

f2d98f00584319df5551666b1578a6a038532578
Bill Yang <joyang12345@gmail.com>
2023-08-07 15:11:29 -0400

    Added query

--- src/main/java/com/example/application/data/repository/QuestionSetRepository.java
+++ src/main/java/com/example/application/data/repository/QuestionSetRepository.java
@@ -2,8 +2,12 @@ package com.example.application.data.repository;
 
 import com.example.application.data.entity.QuestionSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionSetRepository extends JpaRepository<QuestionSet, Long> {
+    @Query("SELECT qs FROM QuestionSet qs LEFT JOIN FETCH qs.questions WHERE qs.name = :name")
+    QuestionSet findByName(@Param("name") String name);
 }

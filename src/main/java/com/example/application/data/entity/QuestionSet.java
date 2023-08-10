913f377734c9ae61e865590673a1b5041fdee636
Bill Yang <joyang12345@gmail.com>
2023-08-10 10:37:56 -0400

    Added getID to QuestionSet

--- src/main/java/com/example/application/data/entity/QuestionSet.java
+++ src/main/java/com/example/application/data/entity/QuestionSet.java
@@ -11,13 +11,13 @@ public class QuestionSet {
 
     @Id
     @SequenceGenerator(name = "question_set_seq", sequenceName = "QUESTION_SET_SEQ", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "question_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "question_set_seq")
     private Long id;
 
     @Column(nullable = false)
     private String name;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     @JoinColumn(name = "question_set_id")
     private Set<Question> questions = new HashSet<>();
 
@@ -40,6 +40,14 @@ public class QuestionSet {
         this.name = name;
         this.questions = questions;
         this.numberOfQuestion = numberOfQuestion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -66,5 +74,4 @@ public class QuestionSet {
         this.numberOfQuestion = numberOfQuestion;
     }
 }
-
 

fddb21f4751cd5d4645321bd482f898aadaf70d0
Bill Yang <joyang12345@gmail.com>
2023-08-16 14:35:11 -0400

    Add searchByName
    -For filterText

--- src/main/java/com/example/application/data/repository/QuestionSetRepository.java
+++ src/main/java/com/example/application/data/repository/QuestionSetRepository.java
@@ -6,8 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionSetRepository extends JpaRepository<QuestionSet, Long> {
     @Query("SELECT qs FROM QuestionSet qs LEFT JOIN FETCH qs.questions WHERE qs.name = :name")
     QuestionSet findByName(@Param("name") String name);
+
+    @Query("SELECT qs FROM QuestionSet qs " +
+            "WHERE lower(qs.name) LIKE lower(concat('%', :searchTerm, '%'))")
+    List<QuestionSet> searchByName(@Param("searchTerm") String searchTerm);
 }
